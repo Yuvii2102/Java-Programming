bc92d29c7a948d62f21eb3df126f10e17f65a15a
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-29 13:39:59 +0530

    Create 5-storage-box.java

---
+++ Variables/5-storage-box.java
@@ -0,0 +1,13 @@
+public class HelloWorld 
+  {
+    public static void main(String args[]) 
+    {
+        
+        int livingRoom=10;
+        int masterBedroom=21;
+        int bathroom=45;
+        System.out.println("livingRoom");
+        System.out.println("masterBedroom");
+        System.out.println("bathRoom");
+    }
+}
