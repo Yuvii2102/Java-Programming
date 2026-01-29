57dc61a32ccea952a5cf2d8661a285d65686ab9b
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-29 20:09:04 +0530

    Create 6-ID-card.java

---
+++ Variables/6-ID-card.java
@@ -0,0 +1,19 @@
+// ID Card 
+
+public class HelloWorld
+  {
+    public static void main(String args[]) 
+    {
+        String name = "Yuvi";
+        String state = "California";
+        char gender = 'M';
+        int age = 21;
+        boolean organDonor = false;
+
+        System.out.println(name);
+        System.out.println(state);
+        System.out.println(gender);
+        System.out.println(age);
+        System.out.println(organDonor);
+    }
+}
