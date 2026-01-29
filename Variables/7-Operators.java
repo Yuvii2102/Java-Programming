038eb739bee52978b70249575b65c2872192fddd
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-29 21:19:30 +0530

    Create 7-Operators.java

---
+++ Variables/7-Operators.java
@@ -0,0 +1,33 @@
+public class Operators 
+{
+    public static void main(String[] args) 
+  {
+        
+        double result;
+        result = 8 + 2;     // result is now 10
+        result = 8 - 2;     // result is now 6
+        result = 8 * 2;     // result is now 16
+        result = 8 / 2;     // result is now 4
+        result = 8 % 3;     // result is now 2
+
+        System.out.println(result); // Ans:- 2.0 // 2.0 as it is double
+    }
+}
+
+
+
+public class Operators 
+{
+    public static void main(String[] args) 
+  {
+        
+        int result;
+        result = 8 + 2;     // result is now 10
+        result = 8 - 2;     // result is now 6
+        result = 8 * 2;     // result is now 16
+        result = 8 / 2;     // result is now 4
+        result = 8 % 3;     // result is now 2
+
+        System.out.println(result); // Ans:- 2  // 2 as it is int datatype
+    }
+}
