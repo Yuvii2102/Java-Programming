261f13d94f273d0708df199d1ec78b77087dd1c6
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-30 19:43:35 +0530

    Create 13-oatmeal.java

---
+++ Control-flow/13-oatmeal.java
@@ -0,0 +1,14 @@
+Q) Let's say you're deciding what to eat for breakfast. You want to make oatmeal, so you look through your pantry to see if you have oats — and you do!
+
+public class Oatmeal 
+{
+  public static void main(String args[]) 
+  {
+      int oats = 2;
+
+      if (oats > 0) 
+      {
+          System.out.println(" Let's make oatmeal today! ");
+      } 
+  }
+}
