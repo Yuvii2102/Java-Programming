f4a10f3654b46727f2bff88dc33df07cf0e40ab7
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-30 17:56:44 +0530

    Create 11-Escape-room.java

---
+++ Variables/11-Escape-room.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Escape
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I ??");
+        String answer = sc.nextLine();
+
+        System.out.println("You answered");
+        System.out.println(answer);
+        System.out.println("Congrats");
+
+    }
+} 
+  
