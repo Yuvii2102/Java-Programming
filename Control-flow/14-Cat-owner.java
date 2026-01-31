88331655a9a733f1b6964f9f5b9cf865ae7244f3
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-31 16:02:26 +0530

    Create 14-Cat-owner.java

---
+++ Control-flow/14-Cat-owner.java
@@ -0,0 +1,19 @@
+public class Cat 
+{
+    public static void main(String[] args) 
+  {
+        boolean isCatOwner = true;
+
+        if (isCatOwner) 
+        {
+            System.out.println("20% off select cat items with code MEOW2025");
+        } 
+        
+        else 
+        
+        {
+            System.out.println("Welcome to the Pets Pets Pets store!");
+        }
+        
+    }
+}
