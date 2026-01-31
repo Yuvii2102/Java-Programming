0b0998bdc08ccc74c9d967b17568686813e10400
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-31 16:26:57 +0530

    Create 16-Dog-owner.java

---
+++ Control-flow/16-Dog-owner.java
@@ -0,0 +1,27 @@
+public class Sample 
+{
+    public static void main(String[] args) 
+  {
+        boolean isCatOwner = true;
+        boolean isDogOwner = false;
+
+        if (isCatOwner) 
+        {
+            System.out.println("Use the code MEOW for 20% off cat items.");
+          
+        } 
+          
+        else if (isDogOwner)
+        {
+          
+            System.out.println("Use the code WOOF for 20% off dog items.");
+        } 
+        
+        else 
+        
+        {
+            System.out.println("Welcome to the Pets Pets Pets store!");
+        }
+        
+    }
+}
